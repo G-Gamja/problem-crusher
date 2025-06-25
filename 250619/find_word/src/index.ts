15430d01e8715b1ab6579272a3e8fffe30863a75
G-Gamja <87967564+G-Gamja@users.noreply.github.com>
2025-06-25 23:04:48 +0900

    ㅁ

--- 250619/find_word/src/index.ts
+++ 250619/find_word/src/index.ts
@@ -14,17 +14,171 @@ const queryWords = queryData
   .split('\n')
   .map((line) => line.trim());
 
-const uniqueOneMilionDataWordsList = [...new Set(oneMilionDataWords)];
+// NOTE BaseNode의 count키와 스트링 충돌이 나서 일단은 이렇게 구현
+type BaseNodeKey = `${string}__key`;
 
-function compareIsNotStartWith(element: string, parsedPrefixList: string[]) {
-  for (let index = 0; index < parsedPrefixList.length; index++) {
-    if (element[index] !== parsedPrefixList[index]) {
-      return true;
+type BaseNode = {
+  count: number;
+  [key: BaseNodeKey]: BaseNode | null;
+};
+
+const linkedList = new Map<string, BaseNode>();
+
+type BeforeKeyType = {
+  [key: BaseNodeKey]: BeforeKeyType;
+};
+
+const 테스트와꾸2 = {
+  a: {
+    등장횟수: 2,
+    다음노드: {
+      a: {
+        등장횟수: 1,
+        다음노드: {
+          p: {
+            등장횟수: 1,
+          },
+        },
+      },
+      p: {
+        등장횟수: 1,
+        다음노드: {
+          p: {
+            등장횟수: 1,
+            다음노드: {
+              l: {
+                등장횟수: 1,
+                다음노드: {
+                  l: {
+                    등장횟수: 1,
+                    다음노드: {
+                      e: {
+                        등장횟수: 1,
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+      i: {
+        등장횟수: 1,
+        다음노드: {
+          r: {
+            등장횟수: 1,
+          },
+        },
+      },
+    },
+  },
+  b: {
+    등장횟수: 1,
+  },
+};
+
+interface BaseNode2 {
+  count: number;
+  next?: Map<string, BaseNode>;
+}
+const linkedList2 = new Map<string, BaseNode2>();
+
+const find = (value: string) => {
+  const letters = value.split('');
+
+  for (let index = 0; index < letters.length; index++) {
+    const element = letters[index];
+    const isExist = linkedList2.has(element);
+
+    if (isExist) {
+      const curNode = linkedList2.get(element);
+      if (curNode?.next) {
+        // note 그럼 넥스트에 있는 모든 노드의 조합을 찾고 넥스트 없을때까지 들어간다음 해당 노드의 카운트와 함꼐 반환하면 될듯?
+      } else {
+        return curNode?.count;
+      }
     }
   }
+};
 
-  return false;
-}
+const append = (target: string, beforeKeys: BeforeKeyType | undefined) => {
+  // note 단어의 첫번째 알파벳
+  if (!beforeKeys) {
+    // note 만약에 타겟키가 이미 등록되어있지 않다면
+    if (linkedList.has(target)) {
+      linkedList.set(target, {
+        count: 1,
+      });
+    }
+    // note 만약에 타겟키가 이미 등록되어있다면
+    const 기존값 = linkedList.get(target);
+    const 덮어쓸값 = {
+      ...기존값,
+      count: 기존값?.count! + 1,
+    };
+
+    linkedList.set(target, 덮어쓸값);
+  } else {
+    const key = Object.keys(beforeKeys)[0];
+
+    const 기존값 = linkedList.get(key)!;
+    const 덮어쓸값 = {
+      ...기존값,
+      count: 기존값?.count! + 1,
+    };
+    linkedList.set(key, 덮어쓸값);
+
+    // note 그리고 남은 것들은?
+  }
+};
+
+// oneMilionDataWords.forEach((word)=>{
+//   const letters = word.split("");
+//   letters.forEach((letter, i)=>{
+//     const key :BaseNodeKey= `${letter}__key`
+//     if(!linkedList.has(key)){
+//       linkedList.set(key)
+//     }
+//     if(linkedList.has(key))
+
+//   })
+// })
+
+// apple, air, aap
+// 리스트의 각 단어를 알파벳별로 쪼개서 결국엔 마지막껄 찾으면 해당 값이 등장순서 아닌가?
+const 테스트와꾸 = {
+  a: {
+    등장횟수: 2,
+    a: {
+      등장횟수: 1,
+      p: {
+        등장횟수: 1,
+      },
+    },
+    p: {
+      등장횟수: 1,
+      p: {
+        등장횟수: 1,
+        l: {
+          등장횟수: 1,
+          l: {
+            등장횟수: 1,
+            e: {
+              등장횟수: 1,
+            },
+          },
+        },
+      },
+    },
+    i: {
+      등장횟수: 1,
+      r: {
+        등장횟수: 1,
+      },
+    },
+  },
+};
 
 function findWord(prefix: string): {
   duration: number;
@@ -32,44 +186,15 @@ function findWord(prefix: string): {
 } {
   const startTime = performance.now();
   const result: string[] = [];
-  const limit = 10;
 
-  const parsedPrefixList: string[] = [];
-
-  for (let index = 0; index < prefix.length; index++) {
-    parsedPrefixList.push(prefix[index]);
-  }
-
-  for (const element of uniqueOneMilionDataWordsList) {
-    if (result.length === limit) {
-      break;
-    }
-
-    const isWrong = compareIsNotStartWith(element, parsedPrefixList);
-    if (isWrong) continue;
-
-    result.push(element);
-  }
-
-  result.sort((a, b) => {
-    const a등장횟수 = oneMilionDataWords.filter((arrItem) => arrItem === a).length;
-    const b등장횟수 = oneMilionDataWords.filter((arrItem) => arrItem === b).length;
-
-    if (a등장횟수 > b등장횟수) return -1;
-
-    if (a등장횟수 < b등장횟수) return 1;
-
-    if (a등장횟수 === b등장횟수) {
-      const a인덱스 = uniqueOneMilionDataWordsList.findIndex((uniqueItem) => uniqueItem === a);
-      const b인덱스 = uniqueOneMilionDataWordsList.findIndex((uniqueItem) => uniqueItem === b);
-
-      if (b인덱스 > a인덱스) return 1;
-      if (b인덱스 < a인덱스) return -1;
-      if (b인덱스 === a인덱스) return 0;
-    }
-
-    return 0;
-  });
+  // note 사전작업: 연결리스트 구현
+  // note   - 각 노드는 해당하는 알파벳, 단어의 위치(이건 필요없을듯?), 해당위치에 등장하는 알파벳의 총갯수(apple, air 이렇게 있으면 첫번째 단어위치에 해당하는 a는 2겠지)
+  // note   -  다음으로 연결되는 알파벳 포인터
+  // note 인풋이 들어오면, 문자별로 쪼갠다?
+  // note 쪼갠거의 첫번쨰 값을 슽
+  // note
+  // note
+  // note
 
   const duration = performance.now() - startTime;
 
